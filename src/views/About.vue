70cf5ddb86ed1ab61cb07c77b6e07018f88f6440
Ahmed-Sh38an <ahmed.sh38an@gmail.com>
2022-11-20 02:24:33 +0200

    Second commit

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h3>Version 1.0</h3>
+        <router-link to="/">Go Back</router-link>
+    </div>
+</template>
+
+<style scoped>
+    div {
+        text-align: center;
+    }
+</style>
